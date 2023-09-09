8a201d0a7df51d75ade792dd31fd802e97b44d7c
Shubham Upadhyay <upadhyay.shubam@thinkfuture.us>
2023-09-09 10:32:04 +0530

    revert

--- __tests__/login.spec.ts
+++ __tests__/login.spec.ts
@@ -8,7 +8,7 @@ describe('Login', () => {
 
   it.each`
     username | password | message
-    ${'wrong_username'}| ${'secret_sauce'} | ${'Epic sadface: Username is required'},
+    ${'wrong_username'}| ${'secret_sauce'} | ${'Epic sadface: Username and password do not match any user in this service'},
     ${'standard_user'}| ${'wrong_password'} | ${'Epic sadface: Username and password do not match any user in this service'},
     ${'locked_out_user'}| ${'secret_sauce'} | ${'Epic sadface: Sorry, this user has been locked out.'}
   `('should display "$message" when username|password is "$username|$password"', async ({ username, password, message }) => {
